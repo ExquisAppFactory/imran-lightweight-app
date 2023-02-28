5f838a35f87273f0a0b1a4eb8e52cc31691e75e8
Imran Abdulmalik <imran.abdulmalik@exquisappfactory.com>
2023-02-28 11:15:03 +0100

    added user authentication

--- user-service/src/controllers/user.ts
+++ user-service/src/controllers/user.ts
@@ -5,6 +5,8 @@ import { withTransaction } from "../util/database";
 import randomString from "random-string";
 import { hashString } from "../util/utils";
 import { validateRequest } from "../util/validation";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const checkJwt = expressjwt({
   secret: process.env.TOKEN_SECRET as string,
@@ -57,4 +59,126 @@ export const createUser = async (req: Request, res: any, next: any) => {
   };
 
   validateRequest(req, res, validationRules.create, handler);
+};
+
+export const authenticateUser = async (req: Request, res: any) => {
+  // check Basic auth header
+  if (
+    !req.headers.authorization ||
+    req.headers.authorization.indexOf("Basic ") === -1
+  )
+    return res.failUnauthorized("Missing Authorization Header");
+
+  // verify auth credentials
+  const base64Credentials = req.headers.authorization.split(" ")[1];
+  const credentials = Buffer.from(base64Credentials, "base64").toString(
+    "ascii"
+  );
+  const [username, password] = credentials.split(":");
+
+  let user = null;
+
+  // check email
+  user = await User.findOne({ email: username });
+
+  // At this point, if user is still undefined then this user doesn't exist
+  if (!user)
+    return res.failUnauthorized(
+      "Invalid email or password",
+      "invalid-credentials"
+    );
+
+  // validate password
+  if (await bcrypt.compare(password, user.password)) {
+    // Credentials are valid.
+    // Create fresh tokens for the client
+    const access_token = jwt.sign(
+      { id: user.idToken },
+      process.env.TOKEN_SECRET!,
+      {
+        expiresIn: process.env.TOKEN_EXPIRY!,
+        algorithm: "HS256",
+      }
+    );
+    const refresh_token = jwt.sign(
+      { refreshId: user.idToken },
+      process.env.TOKEN_SECRET!,
+      {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY!,
+        algorithm: "HS256",
+      }
+    );
+
+    const data = {
+      accessToken: access_token,
+      refreshToken: refresh_token,
+      tokenType: "Bearer",
+      accessTokenExpiresIn: process.env.TOKEN_EXPIRY!,
+      refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRY!,
+    };
+
+    return res.respond(data);
+  } else {
+    return res.failUnauthorized("Invalid username, email or password");
+  }
+};
+
+export const getNewTokens = async (req: Request, res: any) => {
+  // Fetch the id token
+  const idToken = req.auth.refreshId;
+
+  if (!idToken) return res.failUnauthorized("Unauthorized", "invalid_token");
+
+  // Fetch the user
+  const user = await User.findOne({
+    idToken: idToken,
+  });
+
+  if (!user) return res.failUnauthorized("Unauthorized", "invalid_token");
+
+  // Generate new tokens
+  const access_token = jwt.sign(
+    { id: user.idToken },
+    process.env.TOKEN_SECRET!,
+    {
+      expiresIn: process.env.TOKEN_EXPIRY!,
+      algorithm: "HS256",
+    }
+  );
+  const refresh_token = jwt.sign(
+    { refreshId: user.idToken },
+    process.env.TOKEN_SECRET!,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY!,
+      algorithm: "HS256",
+    }
+  );
+
+  const data = {
+    accessToken: access_token,
+    refreshToken: refresh_token,
+    tokenType: "Bearer",
+    accessTokenExpiresIn: process.env.TOKEN_EXPIRY,
+    refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+  };
+
+  return res.respond(data);
+};
+
+export const postAuthentication = async (req: Request, res: any, next: any) => {
+  // Fetch the id token
+  const idToken = req.auth.id;
+
+  if (!idToken) return res.failUnauthorized();
+
+  // Fetch the user
+  const user = await User.findOne(
+    { id_token: idToken },
+    { password: 0, idToken: 0 }
+  );
+
+  if (!user) return res.failUnauthorized();
+
+  req.user = user;
+  next();
 };
--- user-service/src/routes/user.ts
+++ user-service/src/routes/user.ts
@@ -1,18 +1,24 @@
 import { Router } from "express";
 import {
+  authenticateUser,
   checkJwt,
   createUser,
   getAccountAvailability,
+  getNewTokens,
+  postAuthentication,
 } from "../controllers/user";
 
 const router = Router();
 
 router.post("/", createUser);
 router.post("/availability", getAccountAvailability);
+router.get("/authenticate", authenticateUser);
 
 // Validate access/refresh token
 router.use(checkJwt);
 
 // Protected routes
+router.get("/token", getNewTokens);
+router.use(postAuthentication);
 
 export default router;
